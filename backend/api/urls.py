46894877fbf1fd3112d003593c95a172f420cf2b
medigach <144859618+medigach@users.noreply.github.com>
2024-11-19 12:01:34 -0700

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('hello/', views.hello_world, name='hello_world'),
     path("log_event/", views.log_event_view, name="log_event"),
     path("get_logs/", views.get_logs, name="get_logs"),
+    path('account/', views.account_page, name='account_page'),
 ]
