b6b6074273efeedfa5c8212320e34469cded401d
Breakingfast <awniehau@asu.edu>
2024-11-24 17:12:38 -0700

    Fixed logs, fixed backend, it works now

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello_world, name='hello_world'),
-    path("log_event/", views.log_event_view, name="log_event"),
-    path("get_logs/", views.get_logs, name="get_logs"),
+    path('api/login/', views.login_view, name='login'),
+    path('api/hello/', views.hello_world, name='hello_world'),
+    path("api/log_event/", views.log_event_view, name="log_event"),
+    path("api/get_logs/", views.get_logs, name="get_logs"),
+    path('api/account/', views.account_page, name='account_page'),
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -70,6 +72,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
+
+# Allow requests from React frontend
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Database
