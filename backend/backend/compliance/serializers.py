3837724b76b475128f77b270ae91d65f4287a5fa
medigach <144859618+medigach@users.noreply.github.com>
2025-03-03 12:45:13 -0700

    Update serializers.py

--- backend/backend/compliance/serializers.py
+++ backend/backend/compliance/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from .models import ComplianceReport
+
+class ComplianceReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ComplianceReport
+        fields = '__all__'
 
