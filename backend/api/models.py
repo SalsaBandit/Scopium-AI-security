04582bb4c4ab12435b7dc138c4073de6eb1b871c
medigach <144859618+medigach@users.noreply.github.com>
2025-03-03 11:05:20 -0700

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
 class EventLog(models.Model):
     timestamp = models.DateTimeField()
     event = models.CharField(max_length=255)
