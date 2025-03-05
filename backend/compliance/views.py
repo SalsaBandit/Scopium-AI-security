30754d92d4d4e706cd6cb7b2e363db253551e93a
medigach <144859618+medigach@users.noreply.github.com>
2025-03-05 09:16:50 -0700

    Update views.py

--- backend/compliance/views.py
+++ backend/compliance/views.py
@@ -2,9 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import ComplianceReport
 from .serializers import ComplianceReportSerializer
+from .models import ComplianceReport, ComplianceViolation
+
 
 @api_view(['GET'])
 def get_compliance_reports(request):
     reports = ComplianceReport.objects.all()
     serializer = ComplianceReportSerializer(reports, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def get_compliance_violations(request):
+    violations = ComplianceViolation.objects.all()
+    serializer = ComplianceViolationSerializer(violations, many=True)
+    return Response(serializer.data)
+
