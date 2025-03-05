ddab051e6f532c2dfaa7bc9ea77cdf393c6846c1
medigach <144859618+medigach@users.noreply.github.com>
2025-03-05 09:17:13 -0700

    Update urls.py

--- backend/compliance/urls.py
+++ backend/compliance/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import get_compliance_reports
+from .views import get_compliance_violations
+
 
 urlpatterns = [
     path('reports/', get_compliance_reports, name='compliance-reports'),
+    path('violations/', get_compliance_violations, name='compliance-violations'),
 ]
