6e36d4f5de0aae9687806480e5525deb53f88af9
medigach <144859618+medigach@users.noreply.github.com>
2025-03-06 08:42:28 -0700

    Update urls.py

--- backend/compliance/urls.py
+++ backend/compliance/urls.py
@@ -3,11 +3,14 @@ from .views import get_compliance_reports
 from .views import get_compliance_violations
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import add_test_compliance_report
+
 
 
 urlpatterns = [
     path('reports/', get_compliance_reports, name='compliance-reports'),
     path('violations/', get_compliance_violations, name='compliance-violations'),
+    path('add-test-report/', add_test_compliance_report, name='add-test-report'),
 ]
 
 if settings.DEBUG:
