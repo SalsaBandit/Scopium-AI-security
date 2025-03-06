e17ebe12bb26c361eb756653667820cb65fa173f
medigach <144859618+medigach@users.noreply.github.com>
2025-03-06 08:32:51 -0700

    Update urls.py

--- backend/compliance/urls.py
+++ backend/compliance/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import get_compliance_reports
 from .views import get_compliance_violations
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('reports/', get_compliance_reports, name='compliance-reports'),
     path('violations/', get_compliance_violations, name='compliance-violations'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
