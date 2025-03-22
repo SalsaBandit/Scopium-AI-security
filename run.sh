debacdf2f4f4c08a9c017e49334d4139ea92ae76
Breakingfast <awniehau@asu.edu>
2025-03-22 12:08:29 -0700

    Added Log Export Button to JSON, Cleaned up Account Page

--- run.sh
+++
@@ -1,9 +0,0 @@
-#script to run the dashboard incase user is on MacOS to run open terminal and write ./run.sh
-#uncomment the below line to enable logging output once server starts
-#set -x
-cd backend
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver &
-cd ../frontend
-npm start
